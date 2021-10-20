af52f0a3f1605102c1b046aa459ae1626dfa89d2
drmats <drmats@users.noreply.github.com>
2021-10-20 10:30:26 +0200

    app: idiomatic react code.

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,13 @@
  * @copyright Mat. 2021-present
  */
 
+import type { FC } from "react";
 import { useEffect, useState } from "react";
-import { shuffle } from "@xcmats/js-toolbox/array";
 import { delay } from "@xcmats/js-toolbox/async";
+import { inc } from "@xcmats/js-toolbox/math";
+import { shuffle } from "@xcmats/js-toolbox/array";
 import { timeUnit } from "@xcmats/js-toolbox/utils";
+
 import packageInfo from "../package.json";
 
 
@@ -15,16 +18,20 @@ import packageInfo from "../package.json";
 /**
  * ...
  */
-export const App: React.FC = () => {
+export const App: FC = () => {
+    // ...
     const [text, setText] = useState(packageInfo.name);
+    const [tick, setTick] = useState(0);
 
+    // ...
     useEffect(() => {
-        (async () => {
-            await delay(timeUnit.second);
-            setText(shuffle(text.split("")).join(""));
-        })();
-    }, [text]);
+        delay(timeUnit.second).then(() => {
+            setText(t => shuffle(t.split("")).join(""));
+            setTick(t => inc(t) % 2);
+        });
+    }, [tick]);
 
+    // ...
     return (
         <>{text}</>
     );
