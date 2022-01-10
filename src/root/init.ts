c75e6c8ac22ab969f13ac09a7281db3270e12ec7
drmats <drmats@users.noreply.github.com>
2022-01-10 23:46:31 +0100

    init: added mutable subcontext.

--- src/root/init.ts
+++ src/root/init.ts
@@ -6,6 +6,7 @@
  */
 
 /* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-empty-interface */
 
 import { share } from "mem-box";
 
@@ -35,12 +36,15 @@ export default function init (): ({
         // app memory - volatile, imperative context/storage
         ctx = useMemory(),
 
+        // mutable subcontext
+        mut = {},
+
         // redux store with custom middlewares
         store = createReduxStore();
 
 
     // share application-specific variables
-    share({ store });
+    share({ mut, store });
 
     return {
 
@@ -91,10 +95,16 @@ declare global {
     }
 
     /**
+     * Mutable subcontext.
+     */
+    interface Mut {}
+
+    /**
      * Shared memory context.
      */
     interface Ctx {
         logger: Console;
+        mut: Mut;
         store: ReturnType<typeof createReduxStore>;
     }
 
