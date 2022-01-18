4933a01e9047fffe098c9d7c549a7780f90c5ae8
drmats <drmats@users.noreply.github.com>
2022-01-18 11:31:40 +0100

    layout: NavBar - 'useTheme' custom hook usage.

--- src/layout/components/NavBar.tsx
+++ src/layout/components/NavBar.tsx
@@ -14,6 +14,7 @@ import {
     MonoLabelSmall,
 } from "baseui/typography";
 
+import { useTheme } from "~/layout/hooks";
 import {
     Centered,
     RowContainer,
@@ -29,7 +30,8 @@ import packageInfo from "~/../package.json";
  * ...
  */
 export default function NavBar (): JSX.Element {
-    const [css, theme] = useStyletron();
+    const [css] = useStyletron();
+    const theme = useTheme();
     const overrides = {
         Block: { style: { color: theme.colors.white } },
     };
