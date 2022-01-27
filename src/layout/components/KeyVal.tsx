40e5558804d2bec55b4e69927d87aace860471c0
drmats <drmats@users.noreply.github.com>
2022-01-27 12:41:51 +0100

    layout: 'KeyVal' separated out from 'KvInfoBox'.

---
+++ src/layout/components/KeyVal.tsx
@@ -0,0 +1,82 @@
+/**
+ * Two-column key-value component.
+ *
+ * @license BSD-2-Clause
+ * @copyright Mat. 2020-present
+ */
+
+import type { FC } from "react";
+import {
+    styled,
+    useStyletron,
+} from "baseui";
+import { StyledBody } from "baseui/card";
+import { Label3 } from "baseui/typography";
+
+import { TextColumn } from "~/layout/components/Containers";
+
+
+
+
+/**
+ * ...
+ */
+const LL = styled(Label3, ({ $theme }) => ({
+    color: $theme.colors.colorSecondary,
+}));
+const LV = styled(Label3, {
+    fontWeight: 600,
+});
+
+
+
+
+/**
+ * ...
+ */
+const KeyVal: FC<{
+    data: Record<string, string>;
+    valueFieldMaxWidth?: number;
+}> = ({
+    data, valueFieldMaxWidth,
+}) => {
+    const [css] = useStyletron();
+    const entries = Object.entries(data);
+    const valueWidth = `${valueFieldMaxWidth || 100}px`;
+
+    return (
+        <StyledBody
+            className={css({
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: "5px",
+                marginBottom: "0px !important",
+            })}
+        >
+            <TextColumn
+                className={css({
+                    alignItems: "flex-end",
+                    margin: "0px",
+                })}
+            >
+                { entries.map(([k, _]) => (
+                    <LL key={k}>{`${k}:`}</LL>
+                )) }
+            </TextColumn>
+            <TextColumn
+                className={css({
+                    width: valueWidth,
+                    margin: "0px",
+                    overflow: "hidden",
+                })}
+            >
+                { entries.map(([k, v]) => (
+                    <LV key={k}>{v}</LV>
+                )) }
+            </TextColumn>
+        </StyledBody>
+    );
+};
+
+export default KeyVal;
--- src/layout/components/KvInfoBox.tsx
+++ src/layout/components/KvInfoBox.tsx
@@ -6,28 +6,9 @@
  */
 
 import type { FC } from "react";
-import {
-    styled,
-    useStyletron,
-} from "baseui";
-import { StyledBody } from "baseui/card";
-import { Label3 } from "baseui/typography";
 
 import Box from "~/layout/components/Box";
-import { TextColumn } from "~/layout/components/Containers";
-
-
-
-
-/**
- * ...
- */
-const LL = styled(Label3, ({ $theme }) => ({
-    color: $theme.colors.colorSecondary,
-}));
-const LV = styled(Label3, {
-    fontWeight: 600,
-});
+import KeyVal from "~/layout/components/KeyVal";
 
 
 
@@ -41,46 +22,10 @@ const KvInfoBox: FC<{
     valueFieldMaxWidth?: number;
 }> = ({
     data, surfaceStyle, valueFieldMaxWidth,
-}) => {
-    const [css] = useStyletron();
-    const entries = Object.entries(data);
-    const valueWidth = `${valueFieldMaxWidth || 100}px`;
-
-    return (
-        <Box surfaceStyle={surfaceStyle}>
-            <StyledBody
-                className={css({
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "center",
-                    gap: "5px",
-                    marginBottom: "0px !important",
-                })}
-            >
-                <TextColumn
-                    className={css({
-                        alignItems: "flex-end",
-                        margin: "0px",
-                    })}
-                >
-                    { entries.map(([k, _]) => (
-                        <LL key={k}>{`${k}:`}</LL>
-                    )) }
-                </TextColumn>
-                <TextColumn
-                    className={css({
-                        width: valueWidth,
-                        margin: "0px",
-                        overflow: "hidden",
-                    })}
-                >
-                    { entries.map(([k, v]) => (
-                        <LV key={k}>{v}</LV>
-                    )) }
-                </TextColumn>
-            </StyledBody>
-        </Box>
-    );
-};
+}) => (
+    <Box surfaceStyle={surfaceStyle}>
+        <KeyVal data={data} valueFieldMaxWidth={valueFieldMaxWidth} />
+    </Box>
+);
 
 export default KvInfoBox;
